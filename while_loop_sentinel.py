c76e1d04dc737f89005a58c9b00518ca1ad0afc6
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-20 11:35:48 +0800

    Submitting Python Week 04 - Laboratory # 15

---
+++ while_loop_sentinel.py
@@ -0,0 +1,20 @@
+# John Rey Bulfa
+# ITELEC2
+# Laboratory #15 – Guided Coding Exercise:
+# Variables, Literals, and Case-Sensitivity in Python (with Naming Conventions)
+
+def main():
+    total_sum = 0
+    while True:
+        user_input = input("Enter a number (or 'stop' to finish): ")
+        if user_input.strip().lower() == "stop":
+            break  # Exit the loop
+        try:
+            number = float(user_input)
+            total_sum += number
+        except ValueError:
+                print("Invalid input. Please enter a numeric value or 'stop'.")
+        print("The total sum is:", total_sum)
+
+if __name__ == "__main":
+    main()
