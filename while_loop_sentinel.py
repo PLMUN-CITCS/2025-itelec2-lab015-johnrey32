f685a0157f7fb8e0c5117c3d6fa9d920db7a5ac7
johnrey32 <bulfajohnrey_bsit@plmun.edu.ph>
2025-02-20 11:59:11 +0800

    Submitting Python Week 04 - Laboratory # 15

--- while_loop_sentinel.py
+++ while_loop_sentinel.py
@@ -5,6 +5,7 @@
 
 def main():
     total_sum = 0
+
     while True:
         user_input = input("Enter a number (or 'stop' to finish): ")
         if user_input.strip().lower() == "stop":
@@ -13,7 +14,8 @@ def main():
             number = float(user_input)
             total_sum += number
         except ValueError:
-                print("Invalid input. Please enter a numeric value or 'stop'.")
+            print("Invalid input. Please enter a numeric value or 'stop'.")
+            
     print("The total sum is:", total_sum)
 
 if __name__ == "__main__":
